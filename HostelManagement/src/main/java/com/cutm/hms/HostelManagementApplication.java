590c96283da759dfcf305f693194fda8da8398a4
Ashish@ <akuniverse141@gmail.com>
2018-11-30 15:07:32 +0530

    MY First Commit

---
+++ HostelManagement/src/main/java/com/cutm/hms/HostelManagementApplication.java
@@ -0,0 +1,12 @@
+package com.cutm.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelManagementApplication.class, args);
+	}
+}
